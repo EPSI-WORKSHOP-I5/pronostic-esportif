1f708f65eed47f489a33bea5e5f33998afcd92e2
LevFlavien <levesque.flavien@outlook.fr>
2018-11-28 15:26:22 +0100

    Déplacement des routes de api à web pour l'authentification.

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,49 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('/games')->group(function () {
+    Route::get('/', 'GamesController@getAll');
+    Route::post('/', 'GamesController@create');
+    Route::put('/{game}', 'GamesController@update');
+
+    Route::prefix('/{game}')->group(function () {
+        Route::get('/', 'GamesController@getAll');
+
+        Route::prefix('/tournaments')->group(function () {
+            Route::get('/', 'TournamentsController@getAll');
+            Route::post('/', 'TournamentsController@create');
+        });
+
+        Route::prefix('/teams')->group(function () {
+            Route::get('/', 'TeamsController@getAll');
+            Route::post('/', 'TeamsController@create');
+        });
+    });
+});
+
+Route::prefix('/tournaments')->group(function () {
+
+    Route::prefix('/{tournament}')->group(function () {
+        Route::get('/', 'TournamentsController@get');
+
+        Route::prefix('matches')->group(function () {
+            Route::get('/', 'MatchesController@getByTournament');
+            Route::post('/', 'MatchesController@create');
+        });
+
+        Route::get('teams', 'TeamsController@getByTournament');
+    });
+
+    Route::put('/{tournament}', 'TournamentsController@update');
+});
+
+Route::prefix('/teams')->group(function () {
+    Route::get('/{team}', 'TeamsController@get');
+    Route::put('/{team}', 'TeamsController@update');
+});
+
+Route::prefix('/matches')->group(function () {
+    Route::get('/{match}', 'MatchesController@get');
+    Route::put('/{match}', 'MatchesController@update');
+});
